ee43d3db2d8e31b6c134839251d5c6208a64e41c
AlexTK2012 <121603651@qq.com>
2019-04-01 21:12:04 +0800

    start core nlp

--- Collector/python/boot_flume.sh
+++ Collector/python/boot_flume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 启动flume 读取twitter数据
-nohup flume-ng agent -c /opt/spark-twitter/twitter-collect -f TwitterToKafka.conf -Dflume.root.logger=DEBUG,console -n a1 >> flume.log 2>&1 &
+nohup flume-ng agent -f /opt/spark-twitter/7305CloudProject/Collector/TwitterToKafka.conf -Dflume.root.logger=DEBUG,console -n a1  >> flume.log 2>&1 &
 
 
