1e809aefe440e25790d4ecfb1c96c62a3c6e6309
Boyang <zbykid@outlook.com>
2019-04-09 16:46:38 +0800

    update

---
+++ StreamProcessorFlink/src/main/java/hk/hku/cloud/KafkaConsumer.java
@@ -0,0 +1,292 @@
+package hk.hku.cloud;
+/**
+ * Created by IntelliJ IDEA
+ * Project: CloudProject
+ * Author: Boyang
+ * Date: 2019-04-09 11:45
+ */
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.*;
+/**
+ *
+ *
+ * @package: hk.hku.cloud
+ * @class: KafkaConsumer
+ * @author: Boyang
+ * @date: 2019-04-09 11:45
+ */
+public class KafkaConsumer {
+    private static final String BOOTSTRAP_SERVERS = "bootstrap.servers";
+    private static final String GROUP_ID = "group.id";
+    private static final String KAFKA_TOPIC = "kafka.topic";
+
+    private static final String BOOTSTRAP_SERVERS_PRODUCER = "bootstrap.servers.producer";
+    private static final String GROUP_ID_PRODUCER = "group.id.producer";
+    private static final String KAFKA_TOPIC_PRODUCER_1 = "kafka.topic.producer1";
+    private static final String KAFKA_TOPIC_PRODUCER_2 = "kafka.topic.producer2";
+    private static final String KAFKA_TOPIC_PRODUCER_3 = "kafka.topic.producer3";
+    public static void run(Context context) throws Exception {
+        // set up the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties propConsumer = new Properties();
+        propConsumer.setProperty("bootstrap.servers", context.getString(BOOTSTRAP_SERVERS));
+        propConsumer.setProperty("group.id", context.getString(GROUP_ID));
+
+        Properties propProducer = new Properties();
+        propProducer.setProperty("bootstrap.servers", context.getString(BOOTSTRAP_SERVERS_PRODUCER));
+        propProducer.setProperty("group.id", context.getString(GROUP_ID_PRODUCER));
+
+
+
+        // get input data
+        DataStream<String> stream = env.addSource(new FlinkKafkaConsumer<>(context.getString(KAFKA_TOPIC), new SimpleStringSchema(), propConsumer));
+
+        // Stream transformations
+        DataStream<Status> tweets = stream.map(new JSONParser()).filter(tweet -> ((tweet.getCreatedAt() != null) && (tweet.getText() != null)));
+
+        // create a stream of GPS information
+        DataStream<Tuple4<String, Double, Double, Long>> geoInfo =
+                tweets.filter(tweet -> (TweetFunctions.getTweetCountry(tweet) != null))
+                        .filter(tweet -> TweetFunctions.getTweetGPSCoordinates(tweet) != null)
+                        .map(new TweetToLocation());
+        //geoInfo.print();
+        geoInfo.addSink(new FlinkKafkaProducer<>(context.getString(KAFKA_TOPIC_PRODUCER_3), new SimpleStringSchema(), propProducer));
+
+        // create a stream of Sentiment Analysis
+        DataStream<Tuple2<Long, String>> sentimentInfo = tweets.map(new TweetToSentiment());
+        //sentimentInfo.print();
+        sentimentInfo.addSink(new ElasticsearchSink<>(config, transportAddresses, new SentimentInserter()));
+
+        // create a stream of hashtags
+        DataStream<Tuple2<Long, String>> hashtagsInfo = tweets.filter(tweet -> TweetFunctions.getHashtags(tweet).length > 0)
+                .map(tweet -> new Tuple2<>(tweet.getCreatedAt().getTime(), TweetFunctions.getHashtagsAsString(tweet)))
+                .returns(new TypeHint<Tuple2<Long, String>>() {});
+        hashtagsInfo.addSink(new ElasticsearchSink<>(config, transportAddresses, new HashtagsInserter()));
+
+        // create a stream of keywords
+        DataStream<Tuple2<Long, String>> keywordsInfo = tweets.filter(tweet -> TweetFunctions.getKeywords(tweet).length > 0)
+                .map(tweet -> new Tuple2<>(tweet.getCreatedAt().getTime(), TweetFunctions.getKeywordsAsString(tweet)))
+                .returns(new TypeHint<Tuple2<Long, String>>() {});
+        keywordsInfo.addSink(new ElasticsearchSink<>(config, transportAddresses, new KeywordsInserter()));
+
+        // execute program
+        env.execute("Java Flink KafkaConsumer");
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.err.println("USAGE:\nKafkaConsumer <configFilePath>");
+            return;
+        }
+        try {
+            String configFileLocation = "kafka.properties";
+            Context context = new Context(configFileLocation);
+            KafkaConsumer.run(context);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    /**
+     * Inserts tweet locations into elasticsearch.
+     */
+    public static class LocationInserter
+            implements ElasticsearchSinkFunction<Tuple4<String, Double, Double, Long>> {
+
+        // construct index request
+        @Override
+        public void process(
+                Tuple4<String, Double, Double, Long> record,
+                RuntimeContext ctx,
+                RequestIndexer indexer) {
+
+            // construct JSON document to index
+            Map<String, String> json = new HashMap<>();
+            json.put("time", record.f3.toString());         // timestamp
+            json.put("geolocation", record.f1+","+record.f2);  // lat,lon pair
+            json.put("country", record.f0.toString());      // country
+
+            IndexRequest rqst = Requests.indexRequest()
+                    .index("locations")        // index name
+                    .type("locationsType")  // mapping name
+                    .source(json);
+
+            indexer.add(rqst);
+        }
+    }
+
+    /**
+     * Inserts tweet sentiments into elasticsearch.
+     */
+    public static class SentimentInserter implements ElasticsearchSinkFunction<Tuple2<Long, String>> {
+
+        // construct index request
+        @Override
+        public void process(
+                Tuple2<Long, String> record,
+                RuntimeContext ctx,
+                RequestIndexer indexer) {
+
+            // construct JSON document to index
+            Map<String, String> json = new HashMap<>();
+            json.put("time", record.f0.toString());         // timestamp
+            json.put("sentiment", record.f1);      // sentiment
+
+            IndexRequest rqst = Requests.indexRequest()
+                    .index("sentiments")        // index name
+                    .type("sentimentsType")  // mapping name
+                    .source(json);
+
+            indexer.add(rqst);
+        }
+    }
+
+    /**
+     * Inserts tweet hashtags into the "twitter-analytics" index.
+     */
+    public static class HashtagsInserter implements ElasticsearchSinkFunction<Tuple2<Long, String>> {
+
+        // construct index request
+        @Override
+        public void process(
+                Tuple2<Long, String> record,
+                RuntimeContext ctx,
+                RequestIndexer indexer) {
+
+            // construct JSON document to index
+            Map<String, String> json = new HashMap<>();
+            json.put("time", record.f0.toString());         // timestamp
+            json.put("hashtags", record.f1);      // hashtags
+
+            IndexRequest rqst = Requests.indexRequest()
+                    .index("hashtags")        // index name
+                    .type("hashtagsType")  // mapping name
+                    .source(json);
+
+            indexer.add(rqst);
+        }
+    }
+
+    /**
+     * Inserts tweet keywords into the "twitter-analytics" index.
+     */
+    public static class KeywordsInserter implements ElasticsearchSinkFunction<Tuple2<Long, String>> {
+
+        // construct index request
+        @Override
+        public void process(
+                Tuple2<Long, String> record,
+                RuntimeContext ctx,
+                RequestIndexer indexer) {
+
+            // construct JSON document to index
+            Map<String, String> json = new HashMap<>();
+            json.put("time", record.f0.toString());         // timestamp
+            json.put("keywords", record.f1);      // keywords
+
+            IndexRequest rqst = Requests.indexRequest()
+                    .index("keywords")        // index name
+                    .type("keywordsType")  // mapping name
+                    .source(json);
+
+            indexer.add(rqst);
+        }
+    }
+
+
+    /**
+     * Maps a tweet to its country, latitude, longitude, and timestamp
+     */
+    public static class TweetToLocation implements MapFunction<Status, Tuple4<String, Double, Double, Long>> {
+        @Override
+        public Tuple4<String, Double, Double, Long> map(Status tweet) throws Exception {
+            return new Tuple4<>(TweetFunctions.getTweetCountry(tweet),
+                    TweetFunctions.getTweetGPSCoordinates(tweet).getLatitude(),
+                    TweetFunctions.getTweetGPSCoordinates(tweet).getLongitude(),
+                    tweet.getCreatedAt().getTime()
+            );
+        }
+    }
+
+    /**
+     * Maps a tweet to its country, latitude, longitude, and timestamp
+     */
+    public static class TweetToSentiment implements MapFunction<Status, Tuple2<Long, String>>, CheckpointedFunction {
+
+        private transient ListState<BasicSentimentAnalysis> modelState;
+
+        private transient BasicSentimentAnalysis model;
+
+        @Override
+        public Tuple2<Long,String> map(Status tweet) throws Exception {
+            Long time = tweet.getCreatedAt().getTime();
+            String text = tweet.getText();
+            String score = this.model.getSentimentLabel(text);
+            return new Tuple2<>(time, score);
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext functionSnapshotContext) throws Exception {
+            // constant model, so nothing to do
+        }
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) throws Exception {
+            ListStateDescriptor<BasicSentimentAnalysis> listStateDescriptor = new ListStateDescriptor<>("model", BasicSentimentAnalysis.class);
+
+            modelState = context.getOperatorStateStore().getUnionListState(listStateDescriptor);
+
+            if (context.isRestored()) {
+                // restore the model from state
+                model = modelState.get().iterator().next();
+            } else {
+                modelState.clear();
+
+                // read the model from somewhere, e.g. read from a file
+                model = new BasicSentimentAnalysis("negative-words.txt", "positive-words.txt");
+
+                // update the modelState so that it is checkpointed from now
+                modelState.add(model);
+            }
+        }
+    }
+
+    /**
+     * Implements the JSON parser provided by twitter4J into Flink MapFunction
+     */
+    public static final class JSONParser implements MapFunction<String, Status> {
+        @Override
+        public Status map(String value) {
+            Status status = null;
+            try {
+                status = TwitterObjectFactory.createStatus(value);
+            } catch(TwitterException e) {
+                // TODO: use LOGGER class and add a small explaining message
+                e.printStackTrace();
+            } finally {
+                return status; // return the parsed tweet, or null if exception occured
+            }
+        }
+    }
+}
---
+++ StreamProcessorFlink/src/main/java/hk/hku/cloud/TweetFunctions.java
@@ -0,0 +1,141 @@
+package hk.hku.cloud;
+/**
+ * Created by IntelliJ IDEA
+ * Project: CloudProject
+ * Author: Boyang
+ * Date: 2019-04-09 11:51
+ */
+import twitter4j.Status;
+import twitter4j.GeoLocation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import java.lang.Math;
+/**
+ *
+ *
+ * @package: hk.hku.cloud
+ * @class: TweetFunction
+ * @author: Boyang
+ * @date: 2019-04-09 11:51
+ */
+public class TweetFunctions {
+    // Provide (the most likely) Country of Origin of the Tweet
+    public static String getTweetCountry(Status tweet) {
+        String country = null;
+
+        /*
+        TODO: use as favorite metric the GPS Coordinates optionnally provided in the tweet (tweet 'coordinates' field) and extract country
+        free Public Domain project that does that: https://github.com/AReallyGoodName/OfflineReverseGeocode
+        or google maps API (rate limited & on the internet = slow)
+         */
+
+        // fallback to place "associated" with tweet (not necessarily true place)
+        if(tweet.getPlace() != null && tweet.getPlace().getCountry() != null) {
+            country = tweet.getPlace().getCountry();
+        }
+
+        // TODO: fallback to user profile location, check that it is not stupid (e.g. 'Mars') and extract country
+        return country;
+    }
+
+    // get the hashtags present in the tweet
+    public static String[] getHashtags(Status tweet) {
+        return Arrays.stream(tweet.getHashtagEntities()).map(hashtagEntity -> hashtagEntity.getText()).toArray(String[]::new);
+    }
+
+    // get the keywords / topics mentionned in the tweet
+    public static String[] getKeywords(Status tweet) {
+        String fulltext = tweet.getText();
+        String[] words = null;
+        // TODO: load a bigger stopword dictionnary from file
+        String[] stopwords = {"", " ", "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "d", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "got", "gonna", "gotta", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ll", "ltd", "m", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "pm", "put", "rather", "re", "rt", "s", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "til", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "ve", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"};
+        ArrayList<String> keywordslist = new ArrayList<>();
+
+        fulltext = fulltext.toLowerCase(); //transform to lowercase
+        fulltext = fulltext.replaceAll("http\\S+", ""); //remove urls
+        fulltext = fulltext.replaceAll("@\\S+", ""); //remove user mentions
+        fulltext = fulltext.replaceAll("\\p{Punct}", ""); //remove punctuation
+        fulltext = fulltext.replaceAll("\\W", " "); //remove non words
+        fulltext = fulltext.replaceAll("[^a-z]", " "); //remove non letter words // TODO: useless?
+        // split on spaces
+        words = fulltext.split("\\s+");
+
+        // remove stop words
+        for(int i = 0; i < words.length; i++)
+        {
+            if(!Arrays.asList(stopwords).contains(words[i])) {
+                keywordslist.add(words[i]);
+            }
+        }
+        String[] keywords = new String[keywordslist.size()];
+        keywords = keywordslist.toArray(keywords);
+
+        return keywords;
+    }
+
+    // give a score to the tweet to measure whether the Author (User) is worth sponsoring
+    // the higher the score, the better
+    public static double getInfluencerScore(Status tweet) {
+        int nbFollowers = tweet.getUser().getFollowersCount();
+        int engagement = TweetFunctions.getEngagementScore(tweet);
+        return Math.sqrt((1+nbFollowers) * (1+engagement)); // geometric mean (with +1 to prevent zeroing)
+    }
+
+    // give a score to how well a tweet is engaging with users of Twitter (popularity of the tweet)
+    // the higher, the better
+    public static int getEngagementScore(Status tweet) {
+        int retweets = tweet.getRetweetCount();
+        int favorites = tweet.getFavoriteCount();
+        /* TODO: Twitter4J doesn't allow to retrieve reply_count or quote_count although I have these fields in my JSONs..
+        -> it seems these fields are not even supposed to be accessible on free Twitter Streaming API (information dated of 18 October 2017):
+        --> weird
+        https://twittercommunity.com/t/reply-count-quote-count-not-available-in-statuses-lookup-answer/95241
+        https://github.com/yusuke/twitter4j/blob/master/twitter4j-core/src/main/java/twitter4j/Status.java
+        https://github.com/yusuke/twitter4j/blob/master/twitter4j-core/src/internal-json/java/twitter4j/StatusJSONImpl.java
+        */
+        return retweets+favorites;
+    }
+
+    public static String getHashtagsAsString(Status tweet) {
+        String[] hashtags = TweetFunctions.getHashtags(tweet);
+        StringBuilder sb = new StringBuilder();
+        for(String hashtag : hashtags) {
+            sb.append(hashtag + " ");
+        }
+        return sb.toString();
+    }
+
+    public static String getKeywordsAsString(Status tweet) {
+        String[] keywords = TweetFunctions.getKeywords(tweet);
+        StringBuilder sb = new StringBuilder();
+        for(String keyword : keywords) {
+            sb.append(keyword + " ");
+        }
+        return sb.toString();
+    }
+
+    public static GeoLocation getTweetGPSCoordinates(Status tweet) {
+        GeoLocation coordinates = null;
+
+        // favorite metric
+        if(tweet.getGeoLocation() != null) {
+            coordinates = tweet.getGeoLocation();
+        }
+
+        // fallback to place "associated" with tweet and extract center GPS coordinates from bounding box
+        else if(tweet.getPlace() != null && tweet.getPlace().getBoundingBoxCoordinates() != null) {
+            GeoLocation[] boundingBox =  tweet.getPlace().getBoundingBoxCoordinates()[0]; // get bounding box
+            // compute center of bounding box
+            double centerLatitude = Arrays.stream(boundingBox).map(location -> location.getLatitude())
+                    .collect(Collectors.averagingDouble(d -> d));
+            double centerLongitude = Arrays.stream(boundingBox).map(location -> location.getLongitude())
+                    .collect(Collectors.averagingDouble(d -> d));
+
+            coordinates = new GeoLocation(centerLatitude, centerLongitude);
+        }
+        return coordinates;
+    }
+}
